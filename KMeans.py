84098fddddb37cec8ad13d23c811c6c0dbdf6a9f
aolmo <aolmoher@uccs.edu>
2019-03-25 17:16:40 -0700

    first commit

---
+++ KMeans.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+df = pd.read_csv("breast-cancer-wisconsin.data")
+to_np = df.iloc[:, 1:-1]
+X = to_np.values
+X[X=='?'] = np.nan
+# X = X.astype(int)
+
+k = 5
+# Initialize centroids
+c_j = np.expand_dims([1]*X.shape[1], axis=0)
+# c_j = np.tile(c_j, k)
+# c_j = np.expand_dims(c_j.reshape([-1, 9]), axis=1)
+
+print(c_j.shape)
+
+x_i = X[:10]
+# x_i = np.tile(x_i, k)
+# x_i = np.expand_dims(np.reshape(x_i, [-1, 9]), axis=0)
+
+print(x_i.shape)
+
+
+data_point = tf.placeholder("float", [None, 9])
+centroid = tf.placeholder("float", [None, 9])
+
+eucl_dist = tf.sqrt(tf.reduce_sum(tf.pow((centroid-data_point), 2), reduction_indices=1))
+
+with tf.Session() as sess:
+    print(sess.run(eucl_dist, feed_dict={centroid: c_j, data_point: x_i}))
+
+
+
+# Assign closest centroid to each point
+# Recalculate centroids from mean
