5ad5b8ec2980be462434ba6896eeb4fb310f2898
aolmo <aolmoher@uccs.edu>
2019-03-25 17:48:02 -0700

    Version before trying to change dims

--- KMeans.py
+++ KMeans.py
@@ -8,20 +8,19 @@ X = to_np.values
 X[X=='?'] = np.nan
 # X = X.astype(int)
 
-k = 5
+k = 3
 # Initialize centroids
 c_j = np.expand_dims([1]*X.shape[1], axis=0)
 # c_j = np.tile(c_j, k)
 # c_j = np.expand_dims(c_j.reshape([-1, 9]), axis=1)
 
-print(c_j.shape)
-
-x_i = X[:10]
+x_i = X[:2]
 # x_i = np.tile(x_i, k)
 # x_i = np.expand_dims(np.reshape(x_i, [-1, 9]), axis=0)
 
-print(x_i.shape)
-
+np.random.seed(0)
+# centroids = np.ones((k, 9))
+centroids = np.random.randint(11, size=(k, 9))
 
 data_point = tf.placeholder("float", [None, 9])
 centroid = tf.placeholder("float", [None, 9])
@@ -29,7 +28,9 @@ centroid = tf.placeholder("float", [None, 9])
 eucl_dist = tf.sqrt(tf.reduce_sum(tf.pow((centroid-data_point), 2), reduction_indices=1))
 
 with tf.Session() as sess:
-    print(sess.run(eucl_dist, feed_dict={centroid: c_j, data_point: x_i}))
+    for c_j in centroids:
+        c_j = np.expand_dims(c_j, axis=0)
+        print(sess.run(eucl_dist, feed_dict={centroid: c_j, data_point: x_i}))
 
 
 
