3021c937216368abd6e2f67be27ffefbfd0525ae
aolmo <aolmoher@uccs.edu>
2019-03-25 18:37:34 -0700

    Assignation of each datapoint to its cluster

--- KMeans.py
+++ KMeans.py
@@ -2,23 +2,19 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
+np.random.seed(0)
+
 df = pd.read_csv("breast-cancer-wisconsin.data")
 to_np = df.iloc[:, 1:-1]
 X = to_np.values
 X[X=='?'] = np.nan
 # X = X.astype(int)
 
+n = 3
 k = 3
 # Initialize centroids
-c_j = np.expand_dims([1]*X.shape[1], axis=0)
-# c_j = np.tile(c_j, k)
-# c_j = np.expand_dims(c_j.reshape([-1, 9]), axis=1)
+X = X[:n]
 
-x_i = X[:2]
-# x_i = np.tile(x_i, k)
-# x_i = np.expand_dims(np.reshape(x_i, [-1, 9]), axis=0)
-
-np.random.seed(0)
 # centroids = np.ones((k, 9))
 centroids = np.random.randint(11, size=(k, 9))
 
@@ -27,10 +23,28 @@ centroid = tf.placeholder("float", [None, 9])
 
 eucl_dist = tf.sqrt(tf.reduce_sum(tf.pow((centroid-data_point), 2), reduction_indices=1))
 
+c_dict = {}
+for i in range(k):
+    c_dict[i] = []
+
+dists = np.empty((0, n), int)
 with tf.Session() as sess:
     for c_j in centroids:
         c_j = np.expand_dims(c_j, axis=0)
-        print(sess.run(eucl_dist, feed_dict={centroid: c_j, data_point: x_i}))
+        dists = np.vstack((dists, sess.run(eucl_dist, feed_dict={centroid: c_j, data_point: X})))
+
+    closest_centroids = np.argmin(dists, axis=0)
+
+    # get_centroid_points()
+    for i, cc in enumerate(closest_centroids):
+        c_dict[cc].append(X[i])
+
+    for i in range(k):
+        if (len(c_dict[i]) > 0):
+            c_dict[i] = np.stack(c_dict[i], axis=0)
+
+    print(c_dict)
+
 
 
 
