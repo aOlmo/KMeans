738bb329249661dc12856970f282c469c72a4ea7
aolmo <aolmoher@uccs.edu>
2019-04-07 09:11:45 -0700

    Cleaning the code

--- KMeans.py
+++ KMeans.py
@@ -1,59 +1,55 @@
 import numpy as np
-import pprint
 import pandas as pd
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from pandas import DataFrame
+from sklearn.datasets.samples_generator import make_blobs
+
 
 np.random.seed(0)
 
-X = pd.read_csv("breast-cancer-wisconsin.data").iloc[:, 1:-1].values
-X[X=='?'] = 0
-X = X.astype(float)
-
-cols = X.shape[1]
-
-# By doing this we know that the maximum value
-# that we can obtain for each column is 10
-max_val = int(np.amax(X, axis=0)[0])+1
-
-n = 1000
-k = 10
-X = X[:n]
-
 class KMeans:
     def __init__(self, k, X):
+        self.k = k
         self.X = X
+        self.cols = self.X.shape[1]
+        # By doing this we know that the maximum value
+        # that we can obtain for each column is 10
+        self.max_val = int(np.amax(X, axis=0)[0]) + 1
+
         self.potential_function = 0
         self.c_dict = self.init_c_dict()
-        self.centroids = np.random.randint(max_val, size=(k, cols))
-        self.data_point = tf.placeholder("float", [None, cols])
+        self.centroids = np.random.randint(self.max_val, size=(self.k, self.cols))
+        self.data_point = tf.placeholder("float", [None, self.cols])
 
         self.data_point = tf.where(tf.is_nan(self.data_point), tf.zeros_like(self.data_point), self.data_point)
-        self.centroid = tf.placeholder("float", [None, cols])
+        self.centroid = tf.placeholder("float", [None, self.cols])
         self.eucl_dist = tf.sqrt(tf.reduce_sum(tf.square(self.centroid-self.data_point), reduction_indices=1))
 
     def init_c_dict(self):
         c_dict = {}
-        for i in range(k):
-            c_dict[i] = np.empty((0, cols), float)
+        for i in range(self.k):
+            c_dict[i] = np.empty((0, self.cols), float)
 
         return c_dict
 
     def get_centroid_points(self, closest_centroids):
         self.c_dict = self.init_c_dict()
         for i, cc in enumerate(closest_centroids):
-            self.c_dict[cc] = np.vstack((self.c_dict[cc], X[i]))
+            self.c_dict[cc] = np.vstack((self.c_dict[cc], self.X[i]))
 
-        for i in range(k):
+        for i in range(self.k):
             if (len(self.c_dict[i]) > 0):
                 self.c_dict[i] = np.stack(self.c_dict[i], axis=0)
 
 
     def assign_centroids(self, sess):
-        dists = np.empty((0, X.shape[0]), float)
+        dists = np.empty((0, self.X.shape[0]), float)
 
         for c_j in self.centroids:
             c_j = np.expand_dims(c_j, axis=0)
-            dists = np.vstack((dists, sess.run(self.eucl_dist, feed_dict={self.centroid: c_j, self.data_point: X})))
+            dists = np.vstack((dists, sess.run(self.eucl_dist, feed_dict={self.centroid: c_j, self.data_point: self.X})))
 
         closest_centroids = np.argmin(dists, axis=0)
         self.get_centroid_points(closest_centroids)
@@ -142,7 +138,44 @@ class KMeans:
     def get_dict_and_centroids(self):
         return self.c_dict, self.centroids
 
-if __name__ == '__main__':
+
+
+def test_with_blobs(k, n_samples):
+    # Generate 2d classification dataset
+    X, y = make_blobs(n_samples=n_samples, centers=k, n_features=2)
+
     km = KMeans(k, X)
     km.train()
     d, c = km.get_dict_and_centroids()
+
+    # scatter plot, dots colored by class value
+    df = DataFrame(dict(x=X[:, 0], y=X[:, 1], label=y))
+    colors = {0: 'red', 1: 'blue', 2: 'green', 3: 'yellow'}
+    fig, ax = plt.subplots()
+    grouped = df.groupby('label')
+    for key, group in grouped:
+        group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color=colors[key])
+
+    for c_x, c_y in c:
+        plt.plot([c_x], [c_y], 'kx', markersize=5)
+    plt.show()
+
+if __name__ == '__main__':
+    n = 1000
+    k = 14
+
+    X = pd.read_csv("breast-cancer-wisconsin.data").iloc[:, 1:-1].values
+    X[X == '?'] = 0
+    X = X.astype(float)
+    X = X[:n]
+
+    km = KMeans(k, X)
+    km.train()
+
+    # d, c = km.get_dict_and_centroids()
+
+    k = 4
+    n_samples = 200
+    test_with_blobs(k, n_samples)
+
+
